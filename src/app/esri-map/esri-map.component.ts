efaa10155330b365b0653eb73dad399fe0c72d9f
tvoCGI <52041410+tvoCGI@users.noreply.github.com>
2020-08-20 21:28:26 -0500

    Update esri-map.component.ts

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -11,16 +11,7 @@
   limitations under the License.
 */
 
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  Input,
-  Output,
-  EventEmitter,
-  OnDestroy
-} from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, Input, Output, EventEmitter, OnDestroy, OnChanges} from "@angular/core";
 import { loadModules } from "esri-loader";
 import esri = __esri; // Esri TypeScript Types
 
@@ -29,7 +20,7 @@ import esri = __esri; // Esri TypeScript Types
   templateUrl: "./esri-map.component.html",
   styleUrls: ["./esri-map.component.scss"]
 })
-export class EsriMapComponent implements OnInit, OnDestroy {
+export class EsriMapComponent implements OnInit, OnDestroy, OnChanges {
   @Output() mapLoadedEvent = new EventEmitter<boolean>();
 
   // The <div> where we will place the map
@@ -46,8 +37,10 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   private _basemap = "streets";
   private _loaded = false;
   private _view: esri.MapView = null;
-  public paDamageFeatureLayer;
-  public paDamagePtHeatVal = {
+  public qtLayer;
+  public cfLayer;
+  public sfLayer;
+  public quakePT = {
     type: "heatmap",
     blurRadius: 5,
     colorStops: [
@@ -68,47 +61,8 @@ export class EsriMapComponent implements OnInit, OnDestroy {
         maxPixelIntensity: 55,
         minPixelIntensity: 0
   };
-  public uniqueSitesRender = {
-    type: "unique-value",
-    field: "project_status",
-    defaultSymbol: { 
-      type: "simple-marker",
-      style: "circle",
-      color: [108, 199, 43],
-      size: 10, 
-      outline: {
-        color: [255, 255, 255],
-        width: 1
-      }
-    },
-    uniqueValueInfos: [{
-      value: "0", //project formulation
-      symbol:{
-        type: "simple-marker",
-        style: "circle",
-        //color: [25, 113, 255],
-        color: "#56CCF2",
-        size: 10, 
-        outline: {
-          color: [255, 255, 255],
-          width: 1
-        }
-      }
-    }, {
-      value: "1", //obligated 
-      symbol:{
-        type: "simple-marker",
-        style: "circle",
-        color: [108, 199, 43],
-        size: 10, 
-        outline: {
-          color: [255, 255, 255],
-          width: 1
-        }
-      }
-    }]
-  };
 
+  @Input() layerType: string;
   get mapLoaded(): boolean {
     return this._loaded;
   }
@@ -145,20 +99,55 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   async initializeMap() {
     try {
       // Load the modules for the ArcGIS API for JavaScript
-      const [EsriMap, EsriMapView, FeatureLayer] = await loadModules([
+      const [EsriMap, EsriMapView, FeatureLayer, VectorTileLayer, Basemap] = await loadModules([
         "esri/Map",
         "esri/views/MapView",
-        'esri/layers/FeatureLayer'
+        'esri/layers/FeatureLayer',
+        'esri/layers/VectorTileLayer',
+        'esri/Basemap'
       ]);
-      this.paDamageFeatureLayer = new FeatureLayer({
-        url: "https://uatgis.recovery.pr/webgisdev/rest/services/pa_damage_dev/MapServer/0",
+      let topoVector = new VectorTileLayer({
+        portalItem: {
+          id: "8a2cba3b0ebf4140b7c0dc5ee149549a" 
+        }
+      });
+      // let basemap = new Basemap({
+      //   portalItem: {
+      //     id: "8a2cba3b0ebf4140b7c0dc5ee149549a"  // WGS84 Streets Vector webmap
+      //   }
+      // });
+      let bMap = Basemap.fromId("gray-vector")
+      var basemap = new Basemap({
+        baseLayers: [
+          new VectorTileLayer({
+            //url: "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer",
+            portalItem: {
+              id: "8a2cba3b0ebf4140b7c0dc5ee149549a" 
+            },
+            title: "Basemap"
+          })
+        ],
+        title: "basemap",
+        id: "basemap"
+      });
+      this.qtLayer = new FeatureLayer({
+        url: "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Earthquakes_Since1970/MapServer/0",
         outFields: ["*"],
-        renderer: this.paDamagePtHeatVal
+        //renderer: this.quakePT
       })
-
+      this.cfLayer = new FeatureLayer({
+        url: "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/2",
+        outFields: ["*"],
+        //renderer: this.quakePT
+      })
+      this.sfLayer = new FeatureLayer({
+        url: "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3",
+        outFields: ["*"],
+        //renderer: this.quakePT
+      })
       // Configure the Map
       const mapProperties: esri.MapProperties = {
-        basemap: this._basemap
+        basemap: basemap
       };
 
       const map: esri.Map = new EsriMap(mapProperties);
@@ -172,7 +161,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
       };
 
       this._view = new EsriMapView(mapViewProperties);
-      this._view.map.add(this.paDamageFeatureLayer);
+      this._view.map.add(this.qtLayer);
       await this._view.when();
       return this._view;
     } catch (error) {
@@ -181,6 +170,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    console.log("L-Type ",this.layerType);
     // Initialize MapView and return an instance of MapView
     this.initializeMap().then(mapView => {
       // The map has been initialized
@@ -189,7 +179,32 @@ export class EsriMapComponent implements OnInit, OnDestroy {
       this.mapLoadedEvent.emit(true);
     });
   }
-
+  ngOnChanges(){
+    if(this.layerType === 'state'){
+      this._view.map.removeAll();
+      this._view.map.basemap.baseLayers.forEach((baseLayer) => {
+        baseLayer.visible = false;
+      })
+      //this._view.map.removeAll();
+      this._view.map.add(this.sfLayer)
+    }
+    if(this.layerType === 'county'){
+      this._view.map.removeAll();
+      this._view.map.basemap.baseLayers.forEach((baseLayer) => {
+        baseLayer.visible = false;
+      })
+      //this._view.map.removeAll();
+      this._view.map.add(this.cfLayer)
+    }
+    if(this.layerType === 'dPoint'){
+      this._view.map.removeAll();
+      this._view.map.basemap.baseLayers.forEach((baseLayer) => {
+        baseLayer.visible = false;
+      })
+      //this._view.map.removeAll();
+      this._view.map.add(this.qtLayer)
+    }
+  }
   ngOnDestroy() {
     if (this._view) {
       // destroy the map view
